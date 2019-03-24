290f7385b1767ad7f94482d2984e725a4c6eadc8
Fabian Kainka <f.kainka@gmail.com>
2019-03-24 10:13:39 +0100

    App Stucts

--- lib/Apps/AppStructs.hpp
+++ lib/Apps/AppStructs.hpp
@@ -106,12 +106,12 @@ apps_s apps_iot18[]= {
 apps_s apps_sh_lp[]= {
         {"k1_p1",iot18_day2,"[K1_P1 - Onboard RGB-Led]","<a href='../app'>Application Page</a><br>Try out diffrent colors on the onboard RGB-LED (WPS disabled)<br>See <a href='http://iot.fkainka.de/day2-2018'>iot.fkainka.de/day2-2018</a> for details"},
         {"k2_p1",iot18_day3,"[K2_P1 - UDP Test]","Test the UDP Communication.<br>The Board will send a Broadcast-Message every 5 seconds.<br>(UDP, WS, WPS)"},
+
+        //Kapitel 2 - Firmware und tests
         {"k2_p2",iot18_day4,"[K2_P2 - UDP Button]","Test the UDP Communication.<br>The Board will send a Broadcast-Message if you press the boot button<br>(UDP, WS)"},
-
-        //{"iot18_day4",iot18_day4,"[IoT Calendar 2018 - Day4]","See <a href='http://iot.fkainka.de/day4-2018'>iot.fkainka.de/day4-2018</a> (UDP, WS, WPS)"},
-
-        //MQTT MAIN
         {"k2_p3",iot18_day11,"[K2_P3 - MQTT Test]","Test the MQTT Connection<br>MQTT Websocket Client:<br><a href='http://www.hivemq.com/demos/websocket-client/?'>http://www.hivemq.com/demos/websocket-client</a><br>(MQTT, WS, WPS)"},
+        {"k2_p4",sd_intern_temp_exmpl,"[K2_P4 - Intern Temperatur]","<a href='../app'>Intern Temperatur Application Page (SD)</a><br>Read out the intern temperatur Sensor via AJAX<br>Use with SD-Card and SD-Card files from <a href='http://iot.fkainka.de/XXX'>iot.fkainka.de/XXX</a>"},
+        {"k2_p5",sd_intern_hall_exmpl,"[K2_P5 - Intern Hall]","<a href='../app'>Intern Hall-Sensor Application Page (SD)</a><br>Read out the intern hall sensor via WebSockets<br>Use with SD-Card and SD-Card files from <a href='http://iot.fkainka.de/XXX'>iot.fkainka.de/XXX</a>"},
 
         //Kapitel3 - Node RED
         {"k3_p1",iot18_day5,"[K3_P1 - UDP Button]","Flow: <a href='https://pastebin.com/raw/Y0JdbruK'>https://pastebin.com/raw/Y0JdbruK</a><br>(UDP, WS, WPS)"},
@@ -131,16 +131,16 @@ apps_s apps_sh_lp[]= {
 
         {"k3_p13",iot18_day18,"[K3_P13 - Cheerlights]","Flow: <a href='https://pastebin.com/raw/bCUX8S5n'>https://pastebin.com/raw/bCUX8S5n</a><br>PL9823 (MQTT, WS, WPS)"},
         {"k3_p14",iot18_day24,"[K3_P14 - Bunte Lampe]","Flow: <a href='https://pastebin.com/raw/RaHARWnr'>https://pastebin.com/raw/RaHARWnr</a><br>PL9823 x2 (UDP, WS, WPS)"},
+
+        //{"tvLight",tvLight,"[TV Light]","Use 30 WS2812 LEDs as TV Light"},
+        {"k3_p14b",tvLight,"[K3_P15 - TV Hintergrundbeleuchtung]","<a href='../app'>TV-Licht App Page</a><br>(UDP,WS,WPS)"},
+
         {"k3_p15",iot18_day19,"[K3_P15 - Dioden Messung]","Flow: <a href='https://pastebin.com/raw/hPp0Umx5'>https://pastebin.com/raw/hPp0Umx5</a><br>Diode (MQTT, WS, WPS)"},
-
-
-        //  {"k3_p14",iot18_day19,"[K3_P14 - MQTT Button]","Flow: <a href='https://pastebin.com/raw/Tx66se6u'>https://pastebin.com/raw/Tx66se6u</a><br>MQTT Button (UDP, WS, WPS)"},
         {"k3_p16",iot18_day20,"[K3_P16 - Pflanzenwächter]","Flow: <a href='https://pastebin.com/raw/gQtgRT9C'>https://pastebin.com/raw/gQtgRT9C</a><br>Sensoren (MQTT, WS, WPS)"},
         {"k3_p17",iot18_day22,"[K3_P17 - Klimawächter]","Flow: <a href='https://pastebin.com/raw/DegbLw4h'>https://pastebin.com/raw/DegbLw4h</a><br>Sensoren (MQTT, WS, WPS)"},
 
         {"k3_p18",iot18_day21,"[K3_P18 - Piano]","Flow: <a href='https://pastebin.com/raw/c7KPcgDg'>https://pastebin.com/raw/c7KPcgDg</a><br>Sound (UDP, WS, WPS)"},
         {"k3_p19",iot18_day23,"[K3_P19 - Alarmanlage]","Flow: <a href='https://pastebin.com/raw/AuX41egb'>https://pastebin.com/raw/AuX41egb</a><br>Tilt (MQTT, WS, WPS)"},
-        //  {"k3_p14",iot18_day19,"[K3_P14 - MQTT Button]","Flow: <a href='https://pastebin.com/raw/Tx66se6u'>https://pastebin.com/raw/Tx66se6u</a><br>MQTT Button (UDP, WS, WPS)"},
           {"",dflt_init,""} //marks end of handlers
 };
 
@@ -151,8 +151,6 @@ apps_s apps_examples[]= {
         {"dflt_init",dflt_init,"[init_dflt]","The default Application<br>(Webserver, WPS)"},
         {"onboard_led",onboard_led_exmpl,"[Onboard RGB-LED]","<a href='../app'>Application Page</a><br>Try out diffrent colors on the onboard RGB-LED (WPS disabled)<br>See <a href='http://iot.fkainka.de/day2-2018'>iot.fkainka.de/day2-2018</a> for details"},
 
-        {"sd_intern_temp_exmpl",sd_intern_temp_exmpl,"[Intern Temperatur]","<a href='../app'>Intern Temperatur Application Page (SD)</a><br>Read out the intern temperatur Sensor via AJAX<br>Use with SD-Card and SD-Card files from <a href='http://iot.fkainka.de/XXX'>iot.fkainka.de/XXX</a>"},
-        {"sd_intern_hall_exmpl",sd_intern_hall_exmpl,"[Intern Hall Sensor]","<a href='../app'>Intern Hall-Sensor Application Page (SD)</a><br>Read out the intern hall sensor via WebSockets<br>Use with SD-Card and SD-Card files from <a href='http://iot.fkainka.de/XXX'>iot.fkainka.de/XXX</a>"},
 
 
 
